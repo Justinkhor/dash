39f5a3722478c2d348d20f1bb4d2ec5f6209054f
Justinkhor <khorchengee@gmail.com>
2021-11-24 10:11:46 +0800

    Updated app.py

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-from dash import Dash, html, dcc
-import requests
+from dash import Dash, html, dcc, Input, Output, State
 import plotly.express as px
+import requests
+import random
 from PIL import Image
 from io import BytesIO
 
-# Use HTTP requests to obtain PNG image (Change this line to other images)
-x = requests.get('https://raw.githubusercontent.com/Justinkhor/dash/main/images/fire.png')
-
-# Retrieve image file
-img = Image.open(BytesIO(x.content))
-
-# Create the figure
-fig = px.imshow(img)
-fig.update_layout(dragmode="drawrect")
+# List of image names
+images = ["fire", "insta", "tesla"]
 
 # Configurations for the dcc Graph
 config = {
@@ -30,9 +24,33 @@ config = {
 app = Dash(__name__)
 
 # Add the layout for the application
-app.layout = html.Div(
-    [html.H3("Obtain PNG Image via HTTP Request", style={'font-family':'Verdana'}), dcc.Graph(figure=fig, config=config),]
+app.layout = html.Div([
+    html.H3("Obtain PNG Image via HTTP Request", style={'font-family':'Verdana'}),
+    html.Button('Generate Image', id='generate', n_clicks=0), 
+    dcc.Graph(id='output', config=config)   
+])
+
+@app.callback(
+    Output('output', 'figure'),
+    Input('generate', 'n_clicks')
 )
+
+def update_output(n_clicks):
+
+    # Randomly choose one image
+    img = random.choice(images)
+    site = 'https://raw.githubusercontent.com/Justinkhor/dash/main/images/' + img + '.png'
+
+    # Use HTTP requests to obtain PNG image
+    req = requests.get(site)
+
+    # Retrieve image file
+    img = Image.open(BytesIO(req.content))
+
+    fig = px.imshow(img)
+    fig.update_layout(dragmode="drawrect")
+
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
